193d5fef53ed5afd749db00d92840c90783597a2
sebikawa32 <tpqlsdl150@naver.com>
2025-05-25 02:59:12 +0900

    feat: 목록보기/ 추가하기 api 완료

--- src/main/java/com/jose/ticket/domain/ticketinfo/controller/TicketController.java
+++ src/main/java/com/jose/ticket/domain/ticketinfo/controller/TicketController.java
@@ -1,15 +1,17 @@
-package com.jose.ticket.domain.ticketinfo.controller;
+package com.jose.ticket.domain.tickets.controller;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.jose.ticket.domain.ticketinfo.dto.TicketResponseDto;
-import com.jose.ticket.domain.ticketinfo.service.TicketService;
+import com.jose.ticket.domain.tickets.dto.TicketResponseDto;
+import com.jose.ticket.domain.tickets.service.TicketService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
+
+//이 패키지는 티켓 목록을 불러옵니다만..
 
 @RestController
 @RequestMapping("/api/tickets") // 이 컨트롤러는 "/api/tickets"로 시작하는 URL 처리
